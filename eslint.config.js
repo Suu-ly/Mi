ac200220249c89a06431ac9fa17358248958d87c
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2024-11-11 22:52:48 +0800

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import js from "@eslint/js";
-import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tailwind from "eslint-plugin-tailwindcss";
@@ -11,7 +10,7 @@ export default tseslint.config(
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.recommended,
       ...tailwind.configs["flat/recommended"],
     ],
     files: ["**/*.{ts,tsx}"],
@@ -22,12 +21,9 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
